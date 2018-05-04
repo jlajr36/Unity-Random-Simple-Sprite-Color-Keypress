fcb496964e97feabc04b07daca7af85a38a90669
John Amos <jlajr36@gmail.com>
2018-05-04 10:10:44 -0400

    Add files via upload

---
+++ Assets/RandomColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour {
+	
+	public Color[] ColorBank;
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R)) {
+			int num = Random.Range(0, ColorBank.Length);
+			SpriteRenderer sr = GetComponent<SpriteRenderer>();
+			sr.color = ColorBank[num];
+		}
+	}
+}
